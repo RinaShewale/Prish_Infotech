3175c567da6c39778ca151fa994ccc48d684fd72
rinashewale111 <rinashewale6@gmail.com>
2026-09-01 23:09:21 +0530

    fix: Correct import path for User model in passport configuration

--- Backend/src/config/passport.js
+++ Backend/src/config/passport.js
@@ -6,7 +6,7 @@ import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import dotenv from "dotenv";
 
-import User from "../models/user.model.js";
+import User from "../models/User.model.js";
 
 dotenv.config();
 
